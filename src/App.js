5dde7e9c9f456de1be567175101dbbdb9e8bc4da
Paul Gilbert <gilbert.paul@gmail.com>
2022-04-08 11:02:28 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./styles.css";
+import React, { useState } from "react";
+
+const initialResult = "";
 
 export default function App() {
+  const [result, setResult] = useState(initialResult);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="bodyContainer">
+        <div className="buttonContainer">
+          <button onClick={() => handleDieRoll(4)}>1d4</button>
+          <button onClick={() => handleDieRoll(6)}>1d6</button>
+          <button onClick={() => handleDieRoll(8)}>1d8</button>
+          <button onClick={() => handleDieRoll(10)}>1d10</button>
+          <button onClick={() => handleDieRoll(12)}>1d12</button>
+          <button onClick={() => handleDieRoll(20)}>1d20</button>
+          <button onClick={() => handleDieRoll(100)}>1d100</button>
+        </div>
+        <div id="result" className="container">
+          {result}
+        </div>
+      </div>
+    </>
   );
+
+  function handleDieRoll(dieSides) {
+    let result = RollADie(dieSides);
+    console.log(result);
+    setResult(result);
+  }
+
+  function RollADie(max) {
+    return Math.floor(Math.random() * max) + 1;
+  }
 }
